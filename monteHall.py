9d33b554e00e2d5e3483086f592d92beee1d327a
Jeffrey Marple <36581543+Omega-Zero@users.noreply.github.com>
2019-12-16 16:46:42 -0500

    Add files via upload
    initial commit

---
+++ monteHall.py
@@ -0,0 +1,10 @@
+#Monte Hall Simulation(because curious)
+#People say you have a 2/3rd chance of winning if you switch doors after you choose
+#Your first door and a "poop" door is revealed 
+
+def monteSimStay():
+    return 0
+
+
+def monteSimOther():
+    return 0
