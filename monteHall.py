89b8e102a052e2774dce0902d9b3d5f298474759
Jeffrey Marple <36581543+Omega-Zero@users.noreply.github.com>
2019-12-17 13:01:20 -0500

    Add files via upload
    runs sim and returns wins/loss/win percent for simulation where you keep your first choice

--- monteHall.py
+++ monteHall.py
@@ -7,30 +7,64 @@ def montesimstay(rounds):
 
     wins = 0
     losses = 0
-    
-    door1 = False
-    door2 = False
-    door3 = False
+            
+    for currentround in range(rounds): 
 
-    choice = 0    
-         
-    for currentRound in range(rounds): 
+        choice = 0
 
-        #Chooses wining door
-        if door1 == False:
-            if random.randint(0, 1) == 1:
-                door1 = True
-        elif door2 == False:
-            if random.randint(0, 1) == 1:
-                door1 = True
+        door1 = False
+        door2 = False
+        door3 = False
+        
+        #Chooses winning door
+        door = random.randint(1, 3)
+        if door == 1:
+            door1 = True
+        elif door == 2:
+            door2 = True
         else:
-             door3 = True
+            door3 = True
 
 
+        
+        #"Eliminate" one door
         choice = random.randint(1, 3)
+        if choice == 1:
+            if door2 == True:
+                door3 = None
+            else:
+                door2 = None
+                
+        elif choice == 2:
+            if door1 == True:
+                door3 = None
+            else:
+                door1 = None
 
-        print (" Wins: " + str(wins) + " Losses " + str(losses) )
+        else:
+            if door1 == True:
+                door2 = None
+            else:
+                door1 = None
 
+        
+        #Win check
+        if choice == 1 and door1 == True:
+            wins = wins + 1
+            
+        elif choice == 2 and door2 == True:
+            wins = wins + 1
+        elif choice == 3 and door3 == True:
+            wins = wins + 1
+        else:
+            losses = losses + 1 
+        
+         
+
+    winpercent = ( wins /(wins+losses) ) * 100    
+
+    print (" Wins: " + str(wins) + " Losses " + str(losses))
+    print ("You won " + str(winpercent)+ "% of the time by staying with your choice")
 
 
 def montesimother(rounds):
