a7970f8e950f9ece4adc0c4c4ca25f11e4ec9067
Jeffrey Marple <36581543+Omega-Zero@users.noreply.github.com>
2019-12-17 10:10:37 -0500

    Add files via upload

--- monteHall.py
+++ monteHall.py
@@ -1,10 +1,42 @@
-#Monte Hall Simulation(because curious)
-#People say you have a 2/3rd chance of winning if you switch doors after you choose
-#Your first door and a "poop" door is revealed 
+# Monte Hall Simulation
+import random
 
-def monteSimStay():
+
+
+def montesimstay(rounds):
+
+    wins = 0
+    losses = 0
+    
+    door1 = False
+    door2 = False
+    door3 = False
+
+    choice = 0    
+         
+    for currentRound in range(rounds): 
+
+        #Chooses wining door
+        if door1 == False:
+            if random.randint(0, 1) == 1:
+                door1 = True
+        elif door2 == False:
+            if random.randint(0, 1) == 1:
+                door1 = True
+        else:
+             door3 = True
+
+
+        choice = random.randint(1, 3)
+
+        print (" Wins: " + str(wins) + " Losses " + str(losses) )
+
+
+
+def montesimother(rounds):
     return 0
 
 
-def monteSimOther():
-    return 0
+
+
+
